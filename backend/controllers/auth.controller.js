c2dd91391326fc5a0faeeeb7da63fab0aee4546a
sam <samkonnagar123@gmail.com>
2025-07-07 23:00:03 +0530

    handleRegisterUser and handleLoginUser controllers completed

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,9 +1,120 @@
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import UserModel from "../models/User.model.js";
+
 const handleRegisterUser = async (req, res) => {
-  res.json({ message: "Feature not added yet!" });
+  const { fullName, email, password } = req.body;
+  // validation - not empty
+  if (
+    [fullName, email, password].some((field) => field?.trim() === "") ||
+    !fullName ||
+    !email ||
+    !password
+  ) {
+    throw new ApiError(400, "All filds are required");
+  }
+
+  // check if user already exist - username and email
+  const existedUser = await UserModel.findOne({
+    email,
+  });
+  if (existedUser) {
+    throw new ApiError(409, "email already exist");
+  }
+
+  // create user object - create entry in db
+  const user = await UserModel.create({
+    name: fullName.trim(),
+    email,
+    password,
+    avatar: null,
+    bio: null,
+  });
+
+  // remove password from response
+  const createdUser = await UserModel.findById(user._id).select(
+    "-password -__v"
+  );
+
+  // check for user creation
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registerimg the user");
+  }
+
+  // send cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  // generateToken
+  const token = await createdUser.generateToken();
+
+  return res
+    .status(201)
+    .cookie("accessToken", token, options)
+    .json(
+      new ApiResponse(
+        201,
+        { user: createdUser, accessToken: token },
+        "User Created Successfully"
+      )
+    );
 };
 
 const handleLoginUser = async (req, res) => {
-  res.json({ message: "Feature not added yet!" });
+  const { email, password } = req.body;
+  if (!email) {
+    throw new ApiError(400, "Email is required");
+  }
+
+  // find the user
+  const user = await UserModel.findOne({
+    email,
+  });
+
+  if (!user) {
+    throw new ApiError(
+      404,
+      "user does not exist, please create an account first"
+    );
+  }
+
+  // check the password
+  if (!password) {
+    throw new ApiError(404, "please enter the password");
+  }
+  const isPasswordValid = await user.isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "password not correct");
+  }
+
+  const loggedInUser = await UserModel.findById(user._id).select(
+    "-password -__v"
+  );
+
+  // send cookie
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  // generateToken
+  const token = await user.generateToken();
+
+  return res
+    .status(200)
+    .cookie("accessToken", token, options)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: loggedInUser,
+          accessToken: token,
+        },
+        "user logged In Successfully"
+      )
+    );
 };
 
 const handleGetUser = async (req, res) => {
@@ -14,9 +125,4 @@ const handleUpdateUser = async (req, res) => {
   res.json({ message: "Feature not added yet!" });
 };
 
-export {
-  handleRegisterUser,
-  handleLoginUser,
-  handleGetUser,
-  handleUpdateUser,
-};
+export { handleRegisterUser, handleLoginUser, handleGetUser, handleUpdateUser };
--- backend/models/User.model.js
+++ backend/models/User.model.js
@@ -1,4 +1,6 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema(
   {
@@ -30,5 +32,30 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 2);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_TOKEN_EXPIRY,
+    }
+  );
+};
 
 export default model("User", userSchema);
