cd0429329a32774be1979e7c919f05d75d780e51
Sam Roy <samkonnagar123@gmail.com>
2025-08-09 12:58:26 +0530

    Enhance notification handling: implement error handling for notification retrieval and deletion, and add functionality to delete all notifications for a user

--- backend/controllers/notifications.controller.js
+++ backend/controllers/notifications.controller.js
@@ -1,9 +1,48 @@
+import NotificationModel from "../models/Notification.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { ApiError } from "../utils/ApiError.js";
+import mongoose from "mongoose";
+
 const handleGetNotificattion = async (req, res) => {
-  res.json({ message: "Get user notifications" });
+  const userId = req.user._id;
+  if (!userId) throw new ApiError(401, "Unauthorized access");
+
+  const notifications = await NotificationModel.find({ toUser: userId })
+    .sort({ createdAt: -1 })
+    .populate("id")
+    .limit(50);
+
+  return res.status(200).json(new ApiResponse(200, notifications));
 };
 
 const handledeleteNotification = async (req, res) => {
-  res.json({ message: "Mark or delete notification" });
+  const notificationId = req.params?.id;
+  if (!notificationId) throw new ApiError(400, "Notification ID is required");
+  const userId = req.user._id;
+  if (!userId) throw new ApiError(401, "Unauthorized access");
+
+  const notification = await NotificationModel.findOneAndDelete({
+    _id: new mongoose.Types.ObjectId(notificationId),
+    toUser: userId,
+  });
+
+  if (!notification)
+    throw new ApiError(404, "Notification not found or unauthorized");
+
+  return res.status(200).json(new ApiResponse(200, {}, "Deleted successfully"));
 };
 
-export { handleGetNotificattion, handledeleteNotification };
+const handleDeleteAllNotifications = async (req, res) => {
+  const userId = req.user._id;
+  if (!userId) throw new ApiError(401, "Unauthorized access");
+  await NotificationModel.deleteMany({ toUser: userId });
+  return res
+    .status(200)
+    .json(new ApiResponse(200, {}, "All notifications deleted"));
+};
+
+export {
+  handleGetNotificattion,
+  handledeleteNotification,
+  handleDeleteAllNotifications,
+};
--- backend/routes/notifications.route.js
+++ backend/routes/notifications.route.js
@@ -2,11 +2,15 @@ import { Router } from "express";
 import {
   handleGetNotificattion,
   handledeleteNotification,
+  handleDeleteAllNotifications,
 } from "../controllers/notifications.controller.js";
+import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
+router.use(verifyJWT);
 
 router.route("/").get(handleGetNotificattion);
 router.route("/:id").delete(handledeleteNotification);
+router.route("/").delete(handleDeleteAllNotifications);
 
 export default router;
